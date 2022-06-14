4f484b81ec514f5e50850d7796d8a3e3e37ae499
Mangesh2042 <mangesh2042@gmail.com>
2022-06-14 14:20:01 +0530

    counter

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import React, { useState } from "react";
+
+const App=()=>{
+  const [counter,setcounter]=useState(0);
+
+  const inc=()=>{
+    setcounter(counter+1) 
+  }
+  const dec=()=>{
+    setcounter(counter-1)
+  }
+
+  const double=()=>{
+    setcounter(counter*2)
+  }
+
+  return (
+    <div>
+     {/* {
+      counter%2===0 ? <h1 className="green">{counter}</h1> : <h1 className="red">{counter}<h1/>
+    }  */}
+
+     <h1>{counter}</h1>
+      <button onClick={inc}>inc</button>
+      <button onClick={dec}>dec</button>
+      <button onClick={double}>double</button>
+    </div>
+  )
+}
+
+ export default App;
